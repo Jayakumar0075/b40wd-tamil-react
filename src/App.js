043fedacc0bc811da5a09b6955dbcf37e5dff40f
jayakumar <jayakumar0075@gmail.com>
2022-12-01 10:06:21 +0530

    User Component created  in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+// App component = view + logic (Declaration)
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const names=["Jayakumar","selvi","subha","Manju",];
+  const users = [
+    {
+      name: "Arjun",
+      pic:"https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=600"
+      
+    },
+    {
+      name: "selva",
+      pic:"https://media.istockphoto.com/id/1074638206/photo/side-view-long-beard-business-man-portrait.jpg?s=612x612&w=is&k=20&c=1ti4s8QpTTET91yt2t1Jokq2GJWIOWhZw1xNcT_jHBY="
+    },
+    {
+      name:"Siva",
+      pic:"https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      
+    },
+  ];
+  return(
+   <div className="App">
+      {/*<User 
+      name="Arul"
+       pic= "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=600"
+      />
+      <User 
+      name="Ajay"
+       pic="https://media.istockphoto.com/id/1074638206/photo/side-view-long-beard-business-man-portrait.jpg?s=612x612&w=is&k=20&c=1ti4s8QpTTET91yt2t1Jokq2GJWIOWhZw1xNcT_jHBY="
+       />
+      <User
+       name="Siva" 
+      pic="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  />*/}
+  {/* <Welcome name="Jayakumar"/>
+   <Welcome name="selvi"/>
+<Welcome name="subha"/>*/}
+{/*
+ { names.map((nm) => (
+  <Welcome name={nm} />
+ ))} */}
+ {users.map((usr) => (
+  <User name={usr.name} pic={usr.pic} />
+ ))}
+</div>
   );
 }
 
+function Welcome({name}) {
+  //console.log(props, typeof props);
+  //const {pic name}= props;// Object destructuring (ES6)
+  return(
+    <section> 
+      <h1 className= "User-name">
+        Hello,<span className="user-first-name"> {name}</span> 🎉🎉
+        </h1>
+      </section>
+  );
+}
+//object destructuring (ES6)
+function User({pic,name}) {
+  //console.log(props, typeof props);
+  //const {pic,name}=props; // object destructuring (ES6)
+  return(
+    <section>
+      <img className="user-profile-pic" src={pic} alt={name}/>  
+      <h1 className= "User-name">
+        Hello,<span className="user-first-name"> {name}</span> 🎉🎉
+        </h1>;
+      </section>
+  );
+}
+
+
+// object destructuring (ES6)
+//user Component (Declartion)
+//function User(props) {
+  //console.log(props, typeof props);
+  //return(
+    //<section>
+      //<img className="user-profile-pic" src={props.pic} alt={props.name}/>  
+      //<h1 className= "User-name">
+        //Hello,<span className="user-first-name"> {props.name}</span> 🎉🎉
+        //</h1>;
+      //</section>
+  //);
+//}
+
 export default App;
+
+
+
